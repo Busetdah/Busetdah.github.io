62b791a374799da131c4afaa36d224d75b475819
Busetdah <apriantowpj@gmail.com>
2022-04-20 12:21:48 +0700

    add some link for random lucky peeps

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+var urls = new Array();
+urls[0] = "https://youtu.be/aCgP8BFjrw4";
+urls[1] = "https://youtu.be/dQw4w9WgXcQ";
+urls[2] = "https://youtu.be/_l13E-Yb6NM?t=98";
+
 document.getElementById("press").addEventListener("click", function(){
-    if (confirm("Are you sure?")) {
+    if (confirm("Hmm.. wanna continue?")) {
         if (confirm("Once again, are you sure?")) {
-            window.location.href="https://www.youtube.com/shorts/aCgP8BFjrw4";
+            var random = Math.floor(Math.random()*urls.length);
+            window.location = urls[random];
           } else {
             alert("Okay you doesn't get into the game ^_^")
           }
